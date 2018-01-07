1cf5c3901c9f2cec087b0c79ec22b56c355daf3d
Knarfux <franck.hild@gmail.com>
2018-01-07 22:22:12 +0100

    Remove useless code

--- src/websocket/events/index.js
+++ src/websocket/events/index.js
@@ -2,41 +2,7 @@ const onRequest = require('./onRequest')
 
 const createEvents = wss => {
   const events = {
-  	onRequest: onRequest(wss),
-    onRequestAccepted: (wsConn, autObj) => {
-      console.log(`Connection accepted for ${autObj.username}`)
-      
-      wsConn.send(message)
-      const player = players.get(autObj.id)
-      const playerMessage = {
-        type: 'playerInit',
-        data: { id: player.id, x: player.x, y: player.y }
-      }
-      wsConn.send(JSON.stringify(playerMessage))
-      broadcastPlayer(wss, wsConn, 'newPlayer', playerMessage)
-
-      wsConn.send(JSON.stringify({
-        type: 'getPlayers',
-        data: players.getAll()
-      }))
-      wsConn.on('message', msg => {
-        player.keys.update(JSON.parse(msg.utf8Data), player)
-      })
-      wsConn.on('close', err => {
-        console.log(`Connection lose for ${autObj.username}`)
-        unregister(autObj.username).then(username => {
-          const leaveMessage = {
-            data: { id: player.id }
-          }
-          broadcastPlayer(wss, wsConn, 'leavingPlayer', leaveMessage)
-          return username
-        }).then(username => {
-          console.log('Remove player: ', username)
-          players.remove(autObj.id)
-        })
-      })
-    }
-
+  	onRequest: onRequest(wss)
   }
   const type = 'update'
   setInterval(() => {
