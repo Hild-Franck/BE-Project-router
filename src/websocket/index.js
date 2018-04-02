59cf556a2e14f7e396233f01df9ebb46930fbc27
Knarfux <franck.hild@gmail.com>
2018-04-03 00:06:45 +0200

    Forward register request

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,17 +1,29 @@
 const http = require('http')
-const logger = require('../logger')
+const express = require('express')
+const request = require('request-promise')
+const bodyParser = require("body-parser")
 
+const logger = require('../logger')
 const createEvents = require('./events')
+
+const authService = process.env.AUTH || "localhost"
+
+const app = express()
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 8080
 
 const createHTTPServer = () => {
-	const server = http.createServer((req, res) => {
-		res.writeHead(404)
-		res.end()
-	})
-
+	const server = http.createServer(app)
 	server.listen(port, () => logger.info(`Server is listening on port ${port}`))
+	
+	app.post('/register', ({ body }, res) => request({
+		uri: `http://${authService}:4242/register`,
+		method: "POST",
+		body,
+		json: true
+	}).then(res.json))
+
 	return server
 }
 
---
+++ test/register-test.js
@@ -0,0 +1,5 @@
+const ava = require('ava')
+
+ava("should be true", t => {
+	t.is(true, true)
+})
