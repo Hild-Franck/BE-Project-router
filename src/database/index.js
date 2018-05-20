d29be5b21c32aa57d92fe99948a1a0d8992b94e1
Knarfux <franck.hild@gmail.com>
2018-05-20 11:23:15 +0200

    Improve logs

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,13 @@
+const consola = require('consola')
 const init = require('./init')
 const storeNewPlayer = require('./storeNewPlayer')
 const config = require('../../config').redis
 
+const logger = consola.withScope('db')
+
 const database = {
 	init: () => init(config).then(db => {
+		logger.success("Database initialized")
 		database.storeNewPlayer = storeNewPlayer(db)
 	}).catch(({ message }) => logger.error(message)),
 	storeNewPlayer: () => {}
--- src/database/storeNewPlayer.js
+++ src/database/storeNewPlayer.js
@@ -1,8 +1,14 @@
+const consola = require('consola')
+
+const logger = consola.withScope('db.storeNewPlayer')
+
 const storeNewPlayer = db => playerData => {
 	db.hmsetAsync(playerData.username,
 		"id", playerData.id,
 		"x", playerData.x,
-		"y", playerData.y)
+		"y", playerData.y).then(data => {
+		logger.success(`Player ${playerData.username} stored`)
+	})
 }
 
 module.exports = storeNewPlayer
